6b07435ef1676ca3effa8c0e9bdd10f7bc3e07e5
ManojBommu <manojnaiduuu17@gmail.com>
2026-04-27 20:24:02 +0530

    Add error handling to property routes

--- backend/routes/propertyRoutes.js
+++ backend/routes/propertyRoutes.js
@@ -1,23 +1,32 @@
 const router = require("express").Router();
-const Property = require("../models/Property");
+const Property = require("../models/property");
 
-/* ADD */
 router.post("/add", async (req,res)=>{
-    const p = new Property(req.body);
-    await p.save();
-    res.json(p);
+    try{
+        const p = new Property(req.body);
+        await p.save();
+        res.json(p);
+    }catch(err){
+        res.status(500).json("Error adding property");
+    }
 });
 
-/* GET ALL */
 router.get("/all", async (req,res)=>{
-    const data = await Property.find();
-    res.json(data);
+    try{
+        const data = await Property.find();
+        res.json(data);
+    }catch(err){
+        res.status(500).json("Error fetching data");
+    }
 });
 
-/* DELETE */
 router.delete("/delete/:id", async (req,res)=>{
-    await Property.findByIdAndDelete(req.params.id);
-    res.json("Deleted");
+    try{
+        await Property.findByIdAndDelete(req.params.id);
+        res.json("Deleted");
+    }catch(err){
+        res.status(500).json("Error deleting");
+    }
 });
 
 module.exports = router;
